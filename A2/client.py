0c4a9ace197b9677e8c85401b2cdfdc809d48027
reanqbow <reanqbow@gmail.com>
2022-02-13 23:48:50 -0500

    add a2

---
+++ A2/client.py
@@ -0,0 +1,22 @@
+import cv2, socket, pickle, os  
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
+serverip="192.168.16.81"
+serverport=6666
+
+cap = cv2.VideoCapture(0)
+while True:    
+    ret,photo = cap.read()    
+    
+    cv2.imshow('streaming', photo)    
+    
+    ret, buffer = cv2.imencode(".jpg", photo,[int(cv2.IMWRITE_JPEG_QUALITY),30])    
+    x_as_bytes = pickle.dumps(buffer)    
+    
+    s.sendto(x_as_bytes,(serverip , serverport))    
+    
+    if cv2.waitKey(10) == 13:        
+        
+          break  
+cv2.destroyAllWindows()
+cap.release()
